b8f5004d5854dbf2055ad8532036aa53bb5b60f5
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-03-12 14:49:08 +0900

    fakeUserAuth, addFakeUsers 追加

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const { authorizeWithGithub } = require('./lib')
+const fetch = require('node-fetch')
 require('dotenv').config()
 
 module.exports = {
@@ -12,7 +13,7 @@ module.exports = {
     allPhotos: (parent, args, { db }) =>
       db.collection('photos')
         .find()
-        .toArrray(),
+        .toArray(),
 
     totalUsers: (parent, args, { db }) =>
       db.collection('users')
@@ -21,7 +22,7 @@ module.exports = {
     allUsers: (parent, args, { db }) =>
       db.collection('users')
         .find()
-        .toArrray()
+        .toArray()
   },
 
   Mutation: {
@@ -72,25 +73,61 @@ module.exports = {
 
       return { user, token: access_token }
     },
+
+    addFakeUsers: async (root, { count }, { db }) => {
+      var randomUserApi = `https://randomuser.me/api/?results=${count}`
+
+      var { results } = await fetch(randomUserApi)
+        .then(res => res.json())
+
+      var users = results.map(r => ({
+        githubLogin: r.login.username,
+        name: `${r.name.first} ${r.name.last}`,
+        avatar: r.picture.thumbail,
+        githubToken: r.login.sha1
+      }))
+
+      await db.collection('users').insertMany(users)
+
+      return users
+    },
+
+    async fakeUserAuth (parent, { githubLogin }, { db }) {
+      var user = await db.collection('users').findOne({ githubLogin })
+
+      if(!user) {
+        throw new Error(`Cannnot find user with githubLogin ${githubLogin}`)
+      }
+
+      return {
+        token: user.githubToken,
+        user
+      }
+    }
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser)
+    id: parent => parent.id || parent._id,
+    url: parent => `/img/photos/${parent._id}.jpg`,
+    postedBy: (parent, args, { db }) => {
+      db.collection('users').findOne({ githubLogin: parent.userID })
     },
-    taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
-                               .map(tag => tag.userID)
-                               .map(userID => users.find(u => u.githubLogin === userID))
-    },
+    // taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
+    //                            .map(tag => tag.userID)
+    //                            .map(userID => users.find(u => u.githubLogin === userID))
+  },
   User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin)
-    },
-    inPhotos: parent => tags
-      .filter(tag => tag.userID === parent.id)
-      .map(tag => tag.photoID)
-      .map(photoID => photos.find(p => p.id === photoID))
+    postedPhotos: (parent, args, { db }) =>
+        db.collection("photos")
+            .find({ userID: parent.githubLogin })
+            .toArray(),
+    // postedPhotos: parent => {
+    //   return photos.filter(p => p.githubUser === parent.githubLogin)
+    // },
+    // inPhotos: parent => tags
+    //   .filter(tag => tag.userID === parent.id)
+    //   .map(tag => tag.photoID)
+    //   .map(photoID => photos.find(p => p.id === photoID))
   },
   // DateTime: new GraphQLScalarType({
   //   name: 'DateTime',
